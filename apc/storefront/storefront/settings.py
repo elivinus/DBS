97515acf71fd125bbb814c5cb3f6376434ebb42f
elivinus <elivinus@gmail.com>
2022-11-12 16:54:18 +0100

    committed after adding postgresql database to the settings.py

--- apc/storefront/storefront/settings.py
+++ apc/storefront/storefront/settings.py
@@ -79,11 +79,21 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+    
+DATABASES = { 
+    'default': { 
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': 'takeawaydb', 
+        'USER': 'takeaway', 
+        'PASSWORD': 'openmindz', 
+        'HOST': '127.0.0.1', 
+        'PORT': '5434', 
+    } 
 }
 
 

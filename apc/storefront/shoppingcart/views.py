c3e3fe28656c9aaa9a31a3538f1358da7bab7308
elivinus <elivinus@gmail.com>
2022-11-16 18:55:55 +0100

    Commited after adding persistence to model

--- apc/storefront/shoppingcart/views.py
+++ apc/storefront/shoppingcart/views.py
@@ -1,7 +1,7 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import ItemList
 from .forms import CreateNewList
 
 # Create your views here.
@@ -10,8 +10,16 @@ def say_hello(request):
 	return render(request,'homepage.html', { 'pagename' : 'home'})
 
 def register_request(request):
-    form = CreateNewList()
-    return render(request,'register.html', {'form':form})
+    
+	if request.method =='POST':
+		form = CreateNewList(request.POST)
+		if form.is_valid():
+			itemName = form.cleaned_data['name']
+			createItem = ItemList(name=itemName)
+			createItem.save
+	else:
+		form = CreateNewList()
+	return render(request,'register.html', {'form':form})
 
 def login_request(request):
     

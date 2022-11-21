d7f2d03641cb7ca7b111795f01edbfa1163445a0
elivinus <elivinus@gmail.com>
2022-11-21 01:22:24 +0100

    commit after view modify

--- apc/storefront/shoppingcart/admin.py
+++ apc/storefront/shoppingcart/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-from .authentication.models import MenuItem, Customer, DeliveryAgent, Staff, Order, OrderDetails, Ingredints, Recipes, Supplier
+from .authentication.models import MenuItem, Customer, DeliveryAgent, Staff, Order, OrderDetail, Ingredint, Recipe, Supplier, Category
 
 # Register your models here.
 
 admin.site.register(MenuItem)
 admin.site.register(Customer)
+admin.site.register(Category)
 admin.site.register(DeliveryAgent)
 admin.site.register(Staff)
 admin.site.register(Order)
-admin.site.register(OrderDetails)
-admin.site.register(Ingredints)
-admin.site.register(Recipes)
+admin.site.register(OrderDetail)
+admin.site.register(Ingredint)
+admin.site.register(Recipe)
 admin.site.register(Supplier)
--- apc/storefront/shoppingcart/authentication/models.py
+++ apc/storefront/shoppingcart/authentication/models.py
@@ -7,9 +7,9 @@ class MenuItem(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     description = models.CharField(max_length= 300)
     image = models.ImageField(upload_to='menu_images/')
-    category = models.ManyToManyField('Category', related_name = 'item')
+    category = models.ManyToManyField('Category', related_name = 'MenuItem')
     alergies = models.CharField(max_length= 200, blank=True)
-    orderDetailsId = models.OneToOneField('OrderDetails',on_delete=models.CASCADE,default=None)
+ #   orderDetailsId = models.OneToOneField('OrderDetail',on_delete=models.CASCADE,default=None)
     
     def _str_(self):
          return self.name
@@ -25,10 +25,11 @@ class Order(models.Model):
     totalAmount = models.IntegerField()
     orderStatus = models.CharField(max_length=50)
     createDate = models.DateTimeField(auto_now_add=True)
-    orderDetailId = models.ForeignKey('OrderDetails', on_delete=models.CASCADE)
-    customerId = models.ForeignKey('Customer', on_delete=models.CASCADE)
-    deliveryAgentId = models.ForeignKey('DeliveryAgent', on_delete=models.CASCADE)
-    staffId = models.ForeignKey('Staff', on_delete=models.CASCADE)
+    items = models.ManyToManyField('MenuItem', related_name='Order', blank=True)
+   # orderDetailId = models.ForeignKey('OrderDetail', on_delete=models.CASCADE)
+   # customerId = models.ForeignKey('Customer', on_delete=models.CASCADE)
+   # deliveryAgentId = models.ForeignKey('DeliveryAgent', on_delete=models.CASCADE)
+   # staffId = models.ForeignKey('Staff', on_delete=models.CASCADE)
     paymentStatus = models.BooleanField(default=False)
 
     def _str_(self):
@@ -72,7 +73,7 @@ class Staff(models.Model):
     def _str_(self):
         return self.name
 
-class OrderDetails(models.Model):
+class OrderDetail(models.Model):
     totalAmount = models.IntegerField()
     createDate = models.DateTimeField(auto_now_add=True)
     orderID = models.ForeignKey('Order', on_delete=models.CASCADE)
@@ -80,17 +81,17 @@ class OrderDetails(models.Model):
     def _str_(self):
         return self.totalAmount
     
-class Ingredints(models.Model):
+class Ingredint(models.Model):
     name = models.CharField(max_length= 200)
     price = models.IntegerField()
     create_date = models.DateTimeField(auto_now_add=True)
-    recipeId = models.ForeignKey('Recipes', on_delete=models.CASCADE, default=None)
+    recipeId = models.ForeignKey('Recipe', on_delete=models.CASCADE, default=None)
     supplierId = models.ForeignKey('Supplier', on_delete=models.CASCADE,default=None)
 
     def _str_(self):
         return self.name
     
-class Recipes(models.Model):
+class Recipe(models.Model):
     name = models.CharField(max_length= 200)
     description = models.CharField(max_length= 200)
     createDate = models.DateTimeField(auto_now_add=True)

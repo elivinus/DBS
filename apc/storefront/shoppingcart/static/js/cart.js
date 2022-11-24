a21866fc3778f029a238a9318db4495ef24861a3
Luker999 <mpdoyle@outlook.com>
2022-11-24 16:17:37 +0000

    update to folder/files and view.py, cart.js, base

--- apc/storefront/shoppingcart/static/js/cart.js
+++ apc/storefront/shoppingcart/static/js/cart.js
@@ -3,16 +3,49 @@ console.log('testing')
 
 
 var updateBtn = document.getElementsByClassName('update-cart');
-
+// loop through all the buttons
 for (var i = 0; i < updateBtn.length; i++) {
     updateBtn[i].addEventListener('click', function(){
-        var menu = this.dataset.menu;
+        var menuname = this.dataset.menu;
         var action = this.dataset.action;
-        console.log('menu:', menu, 'action:', action)
+        console.log('menuname:', menuname, 'action:', action)
+        console.log('USER:', user)
+
+ 
+        if (user == 'AnonymousUser'){
+            console.log('not authenticated')
+        }else{
+            updateUserOrder(menuname, action)
+        }
 
 
     })
 
+}
+
+// function to update the order
+function updateUserOrder(menuname, action){
+    console.log('user is authenticated, sending data...')
+
+    var url = '/update_Item/'
+
+    fetch(url, {
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X-CSRFToken':csrftoken,
+        },
+        body:JSON.stringify({'menuname':menuname, 'action':action})
+    })
+    
+    .then((response) => {
+        return response.json()
+    })
+
+    .then((data) => {
+        location.reload()
+    })
+    
 }
 
 // Path: apc\storefront\shoppingcart\static\js\cart.js

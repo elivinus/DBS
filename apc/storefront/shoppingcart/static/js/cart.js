0da3af5bea1226b287d2a1e0981ba6ce86410f3a
Luker999 <mpdoyle@outlook.com>
2022-11-24 14:58:37 +0000

    cart.js update

--- apc/storefront/shoppingcart/static/js/cart.js
+++ apc/storefront/shoppingcart/static/js/cart.js
@@ -1,1 +1,18 @@
 console.log('testing')
+
+
+
+var updateBtn = document.getElementsByClassName('update-cart');
+
+for (var i = 0; i < updateBtn.length; i++) {
+    updateBtn[i].addEventListener('click', function(){
+        var menu = this.dataset.menu;
+        var action = this.dataset.action;
+        console.log('menu:', menu, 'action:', action)
+
+
+    })
+
+}
+
+// Path: apc\storefront\shoppingcart\static\js\cart.js

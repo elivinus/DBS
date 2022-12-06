8c0bbd66a5a74ff4f12fa8b8a2a9c26b72ee57a2
elivinus <elivinus@gmail.com>
2022-12-06 06:56:27 +0100

    commited after adding signup

--- apc/storefront/shoppingcart/authentication/forms.py
+++ apc/storefront/shoppingcart/authentication/forms.py
@@ -1,7 +1,16 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-class CreateNewCustomer(forms.Form):
-    name = forms.CharField(label='Name', max_length = 200)
-    check = forms.BooleanField(required=False)
-    
-    
+class CreateNewCustomer(UserCreationForm):
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
+    phone_number = forms.IntegerField()
+    home_address = forms.CharField(max_length=254)
+    city = forms.CharField(max_length=30)
+    postcode = forms.CharField(max_length=30)
+    email = forms.EmailField(max_length=254)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name','phone_number','home_address','city','postcode', 'email', 'password1', 'password2', )

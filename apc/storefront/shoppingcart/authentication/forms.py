44744a20c8e67556ddab1c62057a791bacc08b18
elivinus <elivinus@gmail.com>
2022-12-16 08:58:20 +0100

    Committed after customer creation attempt

--- apc/storefront/shoppingcart/authentication/forms.py
+++ apc/storefront/shoppingcart/authentication/forms.py
@@ -2,6 +2,11 @@ from django import forms
 from allauth.account.forms import SignupForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from datetime import datetime
+import logging
+from ..authentication.models import Customer, OrderDetail
+
+log = logging.getLogger(__name__)
 
 class CreateNewCustomer(UserCreationForm):
     first_name = forms.CharField(max_length=30)
@@ -22,7 +27,35 @@ class CustomSignupForm(SignupForm):
     last_name = forms.CharField(required=True)
     
     def signup(self, request, user):
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
-        user.save()
+        customer = Customer()
+        phone_number = request.POST.get('phonenumber')
+        home_address = request.POST.get('homeaddress')
+        city = request.POST.get('city')
+        post_code = request.POST.get('postcode')
+        
+        customer.user = user.first_name
+        customer.phoneNumber = phone_number
+        customer.city = city
+        customer.postcode = post_code
+        customer.homeAddress = home_address
+        customer.createDate = datetime.today()
+        log.info( phone_number  + " : " + home_address + " : "  + customer)
+        customer.save(self)
+        
+    def save(self, request):
+
+        # Ensure you call the parent class's save.
+        # .save() returns a User object.
+        user = super(CustomSignupForm, self).save(request)
+        log.info( user.first_name  + " : " + user.last_name)
+        print( user.first_name  + " : " + user.last_name)
+        # Add your own processing here.
+
+        # You must return the original result.
+        return user
+        
+        
+        
+        
+        
     

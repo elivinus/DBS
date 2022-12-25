3ca87871c03a51a2741ef14eea2cb20c2f41eda3
elivinus <elivinus@gmail.com>
2022-12-25 20:56:40 +0100

    Committed After Creating Insert Query

--- CreateTableQueriesTakeAway.sql
+++ CreateTableQueriesTakeAway.sql
@@ -3,25 +3,63 @@ GO
 
 CREATE TABLE tbl_Category
 (
+CategoryID   varchar(20) not null,
 CategoryName varchar(20) not null,
-PRIMARY KEY (CategoryName),
+PRIMARY KEY (CategoryID),
 )
+
+GO
+CREATE TABLE tbl_Supplier
+(
+SupplierID varchar(30) not null,
+SupplierName varchar(50) not null,
+SupplierEmailAddress varchar(50) not null,
+SupplierMobileNumber varchar(14) not null,
+SupplierAddress  xml (SupplierAddressCollection),
+SupplierCreateDate  date not null,
+PRIMARY KEY (SupplierID),
+)
+GO
+
+CREATE TABLE tbl_Ingredient
+(
+IngredientID varchar(30) not null,
+IngredientName varchar(20) not null,
+IngredientPrice float not null,
+IngredientCreateDate  date not null,
+SupplierID varchar(30) not null,
+PRIMARY KEY (IngredientID),
+FOREIGN KEY (SupplierID) REFERENCES tbl_Supplier (SupplierID),
+)
+GO
+CREATE TABLE tbl_Recipe
+(
+RecipeID   varchar(20) not null,
+RecipeName varchar(50) not null,
+RecipeDescription varchar(100) not null,
+RecipeCreateDate  date not null,
+IngredientID varchar (30),
+PRIMARY KEY (RecipeID),
+FOREIGN KEY (IngredientID) REFERENCES tbl_Ingredient (IngredientID),
+)
+
 GO
 
 CREATE TABLE tbl_MenuItem
 (
 MenuItemID	varchar(30) not null,
 MenuName	varchar(50) not null,
-MenuPrice	int	not null,
-CategoryName	varchar(20) not null,
-MenuDescription	varchar(50) not null,
-MenuImage	image	not null,
-Alegies	varchar(50)	not null,
+MenuPrice	float	not null,
+CategoryID	varchar(20) not null,
+RecipeID	varchar(20) not null,
+MenuDescription	varchar(200) not null,
+MenuImage	image,
+Alegies	varchar(50),
 PRIMARY KEY	(MenuItemID),
-FOREIGN KEY	(CategoryName)	REFERENCES	tbl_Category(CategoryName),
+FOREIGN KEY	(CategoryID)	REFERENCES	tbl_Category(CategoryID),
+FOREIGN KEY	(RecipeID)	REFERENCES	tbl_Recipe(RecipeID),
 )
 Go
-
 
 CREATE TABLE tbl_Customer
 (
@@ -29,7 +67,7 @@ CustomerID	varchar(30) not null,
 CustomerFirstName varchar(20) not null,
 CustomerLastName varchar(20) not null,
 CustomerEmailAddress varchar(50) not null,
-CustomerMobileNumber int not null,
+CustomerMobileNumber varchar(14) not null,
 CustomerPostCode varchar(10) not null,
 CustomerCity varchar(10) not null,
 CustomerHomeAddress varchar(100) not null,
@@ -45,7 +83,7 @@ AgentID	varchar(30) not null,
 AgentFirstName varchar(20) not null,
 AgentLastName varchar(20) not null,
 AgentEmailAddress varchar(50) not null,
-AgentMobileNumber int not null,
+AgentMobileNumber varchar(14) not null,
 AgentPostCode varchar(10) not null,
 AgentCity varchar(10) not null,
 AgentHomeAddress varchar(100) not null,
@@ -60,7 +98,7 @@ StaffID	varchar(30) not null,
 StaffFirstName varchar(20) not null,
 StaffLastName varchar(20) not null,
 StaffEmailAddress varchar(50) not null,
-StaffMobileNumber int not null,
+StaffMobileNumber varchar(14) not null,
 StaffPostCode varchar(10) not null,
 StaffCity varchar(10) not null,
 StaffHomeAddress varchar(100) not null,
@@ -72,7 +110,7 @@ GO
 CREATE TABLE tbl_Order
 (
 OrderDate date not null,
-OrderAmount varchar(50) not null,
+OrderAmount float not null,
 OrderStatus varchar(50) not null,
 OrderID varchar(100) not null,
 PaymentStatus varchar(10) not null,
@@ -84,11 +122,11 @@ FOREIGN KEY (AgentID) REFERENCES tbl_DeliveryAgent (AgentID),
 FOREIGN KEY (StaffID) REFERENCES tbl_Staff (StaffID),
 FOREIGN KEY (CustomerID) REFERENCES tbl_Customer (CustomerID),
 )
-GO
 
+GO
 CREATE TABLE tbl_OrderDetail
 (
-OrderDetailID int not null,
+OrderDetailID varchar(30) not null,
 OrderQuantity int not null,
 TotalAmount float not null,
 OrderDate date not null,
@@ -98,53 +136,5 @@ PRIMARY KEY (OrderDetailID),
 FOREIGN KEY (MenuItemID) REFERENCES tbl_MenuItem (MenuItemID),
 FOREIGN KEY (OrderID) REFERENCES tbl_Order (OrderID),
 )
-GO
 
-CREATE TABLE tbl_Supplier
-(
-SupplierID varchar(30) not null,
-SupplierName varchar(20) not null,
-SupplierEmailAddress varchar(50) not null,
-SupplierMobileNumber int not null,
-SupplierAddress  xml (SupplierAddressCollection),
-SupplierCreateDate  date not null
-PRIMARY KEY (SupplierID),
-)
-GO
 
-CREATE TABLE tbl_Ingredient
-(
-IngredientID varchar(30) not null,
-IngredientName varchar(20) not null,
-IngredientPrice float not null,
-IngredientCreateDate  date not null,
-PRIMARY KEY (IngredientID)
-)
-GO
-CREATE TABLE tbl_Recipe
-(
-RecipeName varchar(20) not null,
-RecipeDescription varchar(100) not null,
-RecipeCreateDate  date not null,
-Ingredient1 varchar (30),
-Ingredient2 varchar (30),
-Ingredient3 varchar (30),
-Ingredient4 varchar (30),
-Ingredient5 varchar (30),
-Ingredient6 varchar (30),
-Ingredient7 varchar (30),
-Ingredient8 varchar (30),
-Ingredient9 varchar (30),
-Ingredient10 varchar (30),
-PRIMARY KEY (RecipeName),
-FOREIGN KEY (Ingredient1) REFERENCES tbl_Ingredient (IngredientID),
-FOREIGN KEY (Ingredient2) REFERENCES tbl_Ingredient (IngredientID),
-FOREIGN KEY (Ingredient3) REFERENCES tbl_Ingredient (IngredientID),
-FOREIGN KEY (Ingredient4) REFERENCES tbl_Ingredient (IngredientID),
-FOREIGN KEY (Ingredient5) REFERENCES tbl_Ingredient (IngredientID),
-FOREIGN KEY (Ingredient6) REFERENCES tbl_Ingredient (IngredientID),
-FOREIGN KEY (Ingredient7) REFERENCES tbl_Ingredient (IngredientID),
-FOREIGN KEY (Ingredient8) REFERENCES tbl_Ingredient (IngredientID),
-FOREIGN KEY (Ingredient9) REFERENCES tbl_Ingredient (IngredientID),
-FOREIGN KEY (Ingredient10) REFERENCES tbl_Ingredient (IngredientID),
-)
